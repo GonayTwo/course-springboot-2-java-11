2fb95697263e71ea6cdf11005ceb07674a4289f5
Daniel Miranda <daniel_miranda97@hotmail.com>
2022-04-15 15:11:20 -0300

    Project created

---
+++ src/main/java/br/dev/danielmiranda/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package br.dev.danielmiranda.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/danielmiranda/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.danielmiranda.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
