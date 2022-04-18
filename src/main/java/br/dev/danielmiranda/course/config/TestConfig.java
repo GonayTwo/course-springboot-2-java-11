24080597d26d2969073fbbe1948d5dc10eaa4359
Daniel Miranda <daniel_miranda97@hotmail.com>
2022-04-18 13:22:13 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/br/dev/danielmiranda/course/config/TestConfig.java
+++ src/main/java/br/dev/danielmiranda/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import br.dev.danielmiranda.course.entities.Category;
 import br.dev.danielmiranda.course.entities.Order;
+import br.dev.danielmiranda.course.entities.OrderItem;
 import br.dev.danielmiranda.course.entities.Product;
 import br.dev.danielmiranda.course.entities.User;
 import br.dev.danielmiranda.course.entities.enums.OrderStatus;
 import br.dev.danielmiranda.course.repositories.CategoryRepository;
+import br.dev.danielmiranda.course.repositories.OrderItemRepository;
 import br.dev.danielmiranda.course.repositories.OrderRepository;
 import br.dev.danielmiranda.course.repositories.ProductRepository;
 import br.dev.danielmiranda.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -70,6 +75,14 @@ public class TestConfig implements CommandLineRunner{
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); 
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice()); 
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice()); 
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
 	}
 	
 }
---
+++ src/main/java/br/dev/danielmiranda/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package br.dev.danielmiranda.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.dev.danielmiranda.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
